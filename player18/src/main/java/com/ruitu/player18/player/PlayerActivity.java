d9103f8fc9571b7bd4b1781588a84f5855307d4e
chinawubin118 <565696580@qq.com>
2019-03-01 15:58:09 +0800

    加入palyer项目-加入音乐播放器2

--- player18/src/main/java/com/ruitu/player18/player/PlayerActivity.java
+++ player18/src/main/java/com/ruitu/player18/player/PlayerActivity.java
@@ -1,15 +1,20 @@
 package com.ruitu.player18.player;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ruitu.arad.base.BaseActivity;
 import com.ruitu.player18.R;
 
-public class PlayerActivity extends AppCompatActivity {
+public class PlayerActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutRes() {
+        return R.layout.activity_player;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_player);
+
     }
 }
